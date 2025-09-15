43d45861ff7a45cb67425465dbad0ed212e5c96b
wesley-cheung-ics4ui-01-25 <166552362+wesley-cheung-ics4ui-01-25@users.noreply.github.com>
2025-09-15 13:03:04 -0400

    Update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -2,6 +2,44 @@ public class PracticeProblem {
 
 	public static void main(String args[]) {
 
+
+	}
+	public static int sum(int num1, int num2) {
+		int result;
+		result = num1 + num2;
+		return result;
+	}
+	public static int difference(int num1, int num2) {
+		int result;
+		result = num1 - num2;
+		return result;
+	}
+	public static double product(double num1, double num2) {
+		double result;
+		result = num1 * num2;
+		return result;
+	}
+	public static String removeFirst(String sentence) {
+		String result;
+		result = sentence.substring(1);
+		return result;
+	}
+	public static int combinedLength(String sentence1, String sentence2) {
+		int result;
+		result = num1.length()+num2.length();
+		return result;
+	}
+	public static int isEven(int num) {
+		return (num%2==0);
+	}
+	public static int isOdd(int num) {
+		return (num%2==0);
+	}
+	public static int isPositive(int num) {
+		return (num>0);
+	}
+	public static int isNegative(int num) {
+		return (num<0);
 	}
 
 	
